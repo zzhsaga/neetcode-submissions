e4cd60fb27222bd0a09fea3cbd28ebe11f6716a9
N1eo <zzhsaga@gmail.com>
2026-05-10 22:29:06 -0700

    Add: balanced-binary-tree - submission-3

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-3.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # we need to retrival all the leaves and track their depth/height
+        # for every node, check the left high and right high
+        # onething is, if one false happen, we should be able to propagate it to root or we use a global varable
+        # I am thinking about if there is a way we dont need to have a helper function
+        # we can use an early terminate global varible such as if not self.result or not root
+        self.valid = True
+        def dfs(root):
+            if not root or not self.valid:
+                return 0
+            l = dfs(root.left)
+            r = dfs(root.right)
+            if abs(l-r) > 1:
+                self.valid = False
+                return -1
+            else:
+                return 1 + max(l,r)
+
+        result = dfs(root)
+        # print(result)
+        return self.valid        
+        # For BFS, again, I dont know since verify one node need both information from left subtree and right subtree, but bfs we pop the parent early
+        # for iteration dfs, similar, we can do finished hashmap for each node
+            
